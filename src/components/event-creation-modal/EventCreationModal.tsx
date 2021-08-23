8edbabd32b972dbde64618cd7b75f01750646b9d
Rimante <rimanter@wix.com>
2021-08-23 13:30:41 +0300

    Open and close modal

--- src/components/event-creation-modal/EventCreationModal.tsx
+++ src/components/event-creation-modal/EventCreationModal.tsx
@@ -2,13 +2,31 @@ import { Button } from "../button";
 import { Close } from "@material-ui/icons";
 import styles from "./EventCreationModal.module.scss";
 import { EventCreationForm } from "./event-creation-form";
+import { useDispatch } from "react-redux";
+import { closeEventModal } from "../../actions/event-modal";
+import { MouseEvent, useRef } from "react";
 
 export const EventCreationModal = () => {
+  const dispatch = useDispatch();
+
+  const modal = useRef<HTMLDivElement>(null);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (modal.current && !modal.current.contains(event.target as Node)) {
+      dispatch(closeEventModal());
+    }
+  };
+
   return (
-    <div className={styles.container}>
-      <div className={styles.modal}>
+    <div
+      className={styles.container}
+      onClick={event => {
+        handleClickOutside(event);
+      }}
+    >
+      <div className={styles.modal} ref={modal}>
         <div className={styles.header}>
-          <Button onClick={() => {}} styleName={"round"}>
+          <Button onClick={() => dispatch(closeEventModal())} styleName={"round"}>
             <Close />
           </Button>
         </div>
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -2,11 +2,15 @@ import styles from "./Sidebar.module.scss";
 import { Button } from "../button";
 import AddIcon from "@material-ui/icons/Add";
 import { MonthCalendar } from "../month-calendar";
+import { useDispatch } from "react-redux";
+import { openEventModal } from "../../actions/event-modal";
 
 export const Sidebar = () => {
+  const dispatch = useDispatch();
+
   return (
     <aside className={styles.sidebar}>
-      <Button onClick={() => {}} styleName={"big"}>
+      <Button onClick={() => dispatch(openEventModal())} styleName={"big"}>
         <AddIcon style={{ marginRight: "8px" }} />
         <span>Create</span>
       </Button>
